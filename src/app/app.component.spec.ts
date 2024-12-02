0e107bd40bb7151b89e59528fb1e6be8cee38813
Jota <jgarmay674@g.educaand.es>
2024-12-02 23:30:25 +0000

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '11a-angular-bases'`, () => {
+  it(`should have as title '02-angular-bases'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('11a-angular-bases');
+    expect(app.title).toEqual('02-angular-bases');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('11a-angular-bases app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('02-angular-bases app is running!');
   });
 });
